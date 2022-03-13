9021bac396c10935d1a50e5911588d0d5ce0d827
Shikhar Sharma <ceoshikhar@gmail.com>
2022-03-13 16:03:36 +0530

    FancyInput stories with versions using `pattern` prop

--- stories/FancyInput.stories.tsx
+++ stories/FancyInput.stories.tsx
@@ -1,15 +1,43 @@
 import React from "react";
 
-import { FancyInput, FancyInputProps } from "../src/index";
+import { FancyInput, FancyInputProps } from "../src";
 
 export default {
     title: "Fancy Input",
     component: FancyInput,
 };
 
-const Template = (args: FancyInputProps) => <FancyInput {...args} />;
+const Template = (args: FancyInputProps) => (
+    <div
+        style={{
+            maxWidth: 360,
+            fontFamily:
+                "Inter, Roobert, Helvetica Neue, Helvetica, Arial, sans-serif",
+        }}
+    >
+        <FancyInput {...args} />
+    </div>
+);
 
-export const Simple = Template.bind({});
-Simple.args = {
+export const AllCharactersAllowed = Template.bind({});
+AllCharactersAllowed.args = {
     length: 5,
+};
+
+export const AlphaNumeric = Template.bind({});
+AlphaNumeric.args = {
+    length: 5,
+    pattern: "^[a-zA-Z0-9_]*$",
+};
+
+export const OnlyAlphabets = Template.bind({});
+OnlyAlphabets.args = {
+    length: 5,
+    pattern: "^[a-zA-Z0-9_]*$",
+};
+
+export const OnlyNumers = Template.bind({});
+OnlyNumers.args = {
+    length: 5,
+    pattern: "[0-9]",
 };
